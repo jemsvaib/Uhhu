6776d89b8bb835badd56c02ce9d69d432c5fae8e
Glitch (jelly-rebel-chanter) <none>
2023-10-30 09:16:35 +0000

    🏃👶 Checkpoint ./public/index.php:62417452/210

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>
+
+<html>
+  <head>
+    <title>bums</title>
+  </head>
+  <body>
+    <?php
+    print("<h1>Hello!</h1>");
+      ?>
+    <p>
+      <?= 10 + 2 : ?>
+    </p>
+  </body>
+</html>
