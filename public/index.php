24e44ebf78bd094ba4dbff6bc09c46ed8aff54e7
Glitch (jelly-rebel-chanter) <none>
2023-10-30 09:40:47 +0000

    🦃🛣 Checkpoint ./public/index.php:62417452/361

--- public/index.php
+++ public/index.php
@@ -7,8 +7,17 @@
     <?php
     print("<h5>Hello!</h5>");
       ?>
-  <table>
-    <tr
+  <table border="2">
+    <tr>
+      <?php 
+      for ($a = 1; $a < 101; $a++)
+      {
+        if ($a % 5 == 0)
+        {
+          echo "<td>$a</td>";
+        }
+      }?>
+    </tr>
     </table>
   </body>
 </html>
